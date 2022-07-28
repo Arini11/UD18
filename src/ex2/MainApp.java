bcbb74f5deb1c30c3eabbaa0014f2961f3bbe5e6
Arnau Mas <57038668+Arini11@users.noreply.github.com>
2022-07-28 16:00:23 +0200

    Update MainApp.java

--- src/ex2/MainApp.java
+++ src/ex2/MainApp.java
@@ -7,7 +7,6 @@ public class MainApp {
 	public static void main(String[] args) {
 		ConnectionDB conn = new ConnectionDB();
 		conn.createDB("ex2");
-		conn.openConnection();
 		conn.createTable("ex2", "Departamentos", "(codigo int PRIMARY KEY, nombre varchar(100), presupuesto int)");
 		conn.createTable("ex2", "Empleados", "(dni varchar(9) PRIMARY KEY, nombre varchar(100), apellidos varchar(255), departamento int,"
 				+ "constraint FK_Departamento FOREIGN KEY (Departamento) REFERENCES Departamentos(codigo))");
@@ -18,8 +17,8 @@ public class MainApp {
 		conn.insertData("ex2", "Empleados", "VALUES(123234877,'Michael','Rogers',14),(152934485,'Anand','Manikutty',14),"
 				+ "(222364883,'Carol','Smith',37),(326587417,'Joe','Stevens',37),(332154719,'Mary-Anne','Foster',14),"
 				+ "(332569843,'George','O Donnell',77),(546523478,'John','Doe',59),(631231482,'David','Smith',77),"
-				+ "(654873219,'Zacary','Efron',59),(745685214,'Eric','Goldsmith',59),(845657233,'Luis','López',14),"
-				+ "(845657245,'Elizabeth','Doe',14),(845657246,'Kumar','Swamy',14),(845657266,'Jose','Pérez',77)");
+				+ "(654873219,'Zacary','Efron',59),(745685214,'Eric','Goldsmith',59),(845657233,'Luis','LÃ³pez',14),"
+				+ "(845657245,'Elizabeth','Doe',14),(845657246,'Kumar','Swamy',14),(845657266,'Jose','PÃ©rez',77)");
 		conn.getValues("ex2", "Departamentos");
 		conn.getValues("ex2", "Empleados");
 		conn.closeConnection();
