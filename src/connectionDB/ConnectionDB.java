99170487557421a36e1f26f83116be430d65a651
Joana <joanajanefdez@gmail.com>
2022-07-28 10:24:21 +0200

    metodos openConnection i closeConnection, arreglos incongruencias

---
+++ src/connectionDB/ConnectionDB.java
@@ -0,0 +1,119 @@
+package connectionDB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+public class ConnectionDB {
+
+	private Connection conexion;
+
+	public void openConnection() {
+
+		// Creo que se tiene que cambiar en url base dades, mysql por el nombre que
+		// tengamos de la bbdd ?
+		String urlBaseDades = "jdbc:mysql://localhost:3306";
+		String user = "root";
+		String pass = "poner cada uno la suya";
+
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conexion = DriverManager.getConnection(urlBaseDades, user, pass);
+			System.out.println("Server connected");
+
+		} catch (SQLException | ClassNotFoundException ex) {
+			System.out.println("No se ha podido conectar a la base de datos");
+			System.out.println(ex);
+		}
+	}
+
+	public void closeConnection() {
+		try {
+			conexion.close();
+			System.out.println("Se ha finalizado la conexion con el servidor");
+		} catch (SQLException ex) {
+			// no ho agafa tal com esta ara (Class.forName) Logger.getLogger((MySQL.class.getName()).log(Level.SEVERE,null,ex);
+			System.out.println(ex);
+		}
+	}
+
+	public void createDB(String name) {
+		try {
+			if (conexion == null)
+				openConnection();
+			String Query = "CREATE DATABASE " + name;
+			Statement st = conexion.createStatement();
+			st.executeUpdate(Query);
+			conexion.close();
+			System.out.println("Se ha creado la base de datos " + name + " de forma exitosa.");
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			System.out.println("Error creando DB.");
+		}
+	}
+
+	public void createTable(String db, String name, String campos) {
+		try {
+			String Querydb = "USE " + db + ";";
+			Statement stdb = conexion.createStatement();
+			stdb.executeUpdate(Querydb);
+
+			String Query = "CREATE TABLE " + name + " " + campos;
+			Statement st = conexion.createStatement();
+			st.executeUpdate(Query);
+			System.out.println("Tabla creada con exito!");
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			System.out.println("Error creando tabla.");
+		}
+	}
+
+	// INSERTAR DATOS EN TABLAS MYSQL
+
+	public void insertData(String db, String nombre_tabla, String campos) {
+		try {
+			String Querydb = "USE " + db + ";";
+			Statement stdb = conexion.createStatement();
+			stdb.executeUpdate(Querydb);
+
+			String Query = "INSERT INTO " + nombre_tabla + " " + campos;
+
+			Statement st = conexion.createStatement();
+			st.executeUpdate(Query);
+
+			System.out.println("Datos almacenados correctamente");
+		} catch (SQLException ex) {
+			System.out.println(ex.getMessage());
+			JOptionPane.showMessageDialog(null, "Error en el almacenamiento");
+		}
+	}
+
+	// OBTENER VALORES MYSQL
+
+	public void getValues(String db, String nombre_tabla, int numCampos) {
+		try {
+			String Querydb = "USE " + db + ";";
+			Statement stdb = conexion.createStatement();
+			stdb.executeUpdate(Querydb);
+
+			String Query = "SELECT * FROM " + nombre_tabla;
+			Statement st = conexion.createStatement();
+			java.sql.ResultSet resultSet;
+			resultSet = st.executeQuery(Query);
+
+			while (resultSet.next()) {
+				while (numCampos-- > 0) {
+					// No tot ser� string
+					// Try catch amb getDouble???
+					System.out.println(resultSet.getString(numCampos));
+				}
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+
+	}
+}
